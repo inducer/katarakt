a0779534fe5c95946e506c91062edbc082aafdd2
Philipp Erhardt <Philipp.Erhardt@informatik.stud.uni-erlangen.de>
2025-10-25 09:56:20 +0200

    Update README, bump up version to 0.3

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 static void print_version() {
-	cout << "katarakt version 0.2" << endl;
+	cout << "katarakt version 0.3" << endl;
 }
 
 static void print_help(char *name) {
